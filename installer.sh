2781cb04ff512c6e164f83a68a5791ec9dcf65d8
rajprins <rajprins@gmail.com>
2021-11-18 14:03:37 +0100

    Minor cosmetic changes to anonsurf script

--- installer.sh
+++ installer.sh
@@ -2,34 +2,36 @@
 
 # Ensure we are being ran as root
 if [ $(id -u) -ne 0 ]; then
-	echo "This script must be ran as root"
+	echo "Sorry, this script must be ran as root"
 	exit 1
 fi
 
 # For upgrades and sanity check, remove any existing i2p.list file
 rm -f /etc/apt/sources.list.d/i2p.list
 
-# Compile the i2p ppa
-echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
-rm /tmp/i2p-debian-repo.key.asc # delete the temp key
-apt-get update # Update repos
 
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install -y libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install -y geoip-bin libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
-fi
 
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+#### Compile the i2p ppa
+# Default config reads repos from sources.list.d
+echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list
+# Get the latest i2p repo pubkey
+wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc
+# Import the key
+apt-key add /tmp/i2p-debian-repo.key.asc
+# delete the temp key
+rm /tmp/i2p-debian-repo.key.asc
+# Update repos
+apt-get update 
 
-# Configure and install the .deb
-dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
-dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+
+#### This will ensure you get updates to the repository's GPG key and other dependencies, just in case
+apt-get install -y secure-delete tor i2p geoip-bin i2p-keyring i2p i2p-router libjbigi-jni
+
+
+### Configure and install the .deb
+# Build the deb package
+dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb
+# Install the packages
+dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) 
 
 exit 0
