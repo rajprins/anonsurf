ad2eed72e16e74f30f2296e28719151c552ac273
Roy Prins <roy@dev-vm01>
2025-07-15 11:16:50 +0200

    Added missing dependency

--- 00-install-deps.sh
+++ 00-install-deps.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 #
 #
-# Dependencies: tor i2p i2p-keyring i2p-router curl bleachbit iptables iptables-persistent zenity
+# Dependencies: tor secure-delete curl bleachbit iptables iptables-persistent zenity
 
 
 BLUE='\033[1;94m'
 GREEN='\033[1;92m'
 RED='\033[1;91m'
 RESETCOLOR='\033[1;00m'
-APTSRC="/etc/apt/sources.list.d/i2p.list"
-KEYFILE="i2p-archive-keyring.gpg"
 
 
 if [ $(id -u) -ne 0 ]; then
@@ -21,28 +19,15 @@ DISTRO=`awk -F'=' '/^ID=/ {print tolower($2)}' /etc/*-release 2> /dev/null`
 echo;echo -e "${GREEN}>>> Detected distribution: ${DISTRO}${RESETCOLOR}"
 
 
-#echo;echo -e "${GREEN}>>> Preparing I2P repository${RESETCOLOR}"
-#if [[ -f ${APTSRC} ]] ; then
-#   rm -f ${APTSRC}
-#fi
-
 ### Ubuntu
 if [ $DISTRO == "ubuntu" ]; then
 	#apt-add-repository ppa:i2p-maintainers/i2p
 	apt update
 #### Debian(-based) systems
 elif [ $DISTRO == "debian" ]; then
-	#apt-get update
-	#apt-get install apt-transport-https lsb-release curl -y
-	#curl -o /tmp/${KEYFILE} https://geti2p.net/_static/${KEYFILE}
-	#cp /tmp/${KEYFILE} /usr/share/keyrings/
-  	#echo "deb [signed-by=/usr/share/keyrings/${KEYFILE}] https://deb.i2p.net/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/i2p.list
   	apt update
 ### Downstream Debian, like Kali, Parrot etc.
 elif [ $DISTRO == "kali" ]; then
-	#curl -o /tmp/${KEYFILE} https://geti2p.net/_static/${KEYFILE}
-	#cp /tmp/${KEYFILE} /usr/share/keyrings/
-  	#echo "deb [signed-by=/usr/share/keyrings/${KEYFILE}] https://deb.i2p.net/ $(dpkg --status tzdata | grep Provides | cut -f2 -d'-') main" | sudo tee /etc/apt/sources.list.d/i2p.list
 	apt update
 else
 	echo "Unsupported Linux distro: $DISTRO. Exiting..."
@@ -50,8 +35,7 @@ else
 fi
 
 echo;echo -e "${GREEN}>>> Installing packages${RESETCOLOR}"
-#apt install zenity tor i2p i2p-keyring i2p-router curl bleachbit iptables iptables-persistent -y
-apt install zenity tor curl bleachbit iptables iptables-persistent -y
+apt install secure-delete zenity tor curl bleachbit iptables iptables-persistent -y
 
 echo;echo "All done."
 exit 0
